1926aa3e2ca4d05853c7246a571bec80fcf1f82c
Alexus Huang <141088068+alexus-Huang@users.noreply.github.com>
2026-09-18 16:08:30 -0700

    Try RandomForestRegressor for comparison (MAE 14.61, no improvement over linear)

--- train_model.py
+++ train_model.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import joblib
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 
@@ -19,8 +19,8 @@ X = df[['cpu_percent','ram_percent']]
 y = df['target_cpu']
 X_train,X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=8)
 
-model = LinearRegression()
-model.fit(X_train,y_train)
+model = RandomForestRegressor(n_estimators=100, random_state=8)
+model.fit(X_train, y_train)
 
 predictions = model.predict(X_test)
 mae = mean_absolute_error(y_test, predictions)
